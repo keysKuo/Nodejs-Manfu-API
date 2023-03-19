63731ec87a584588f058694e67be0816889dac6b
Sheldon Kuo <nkeyskuo124@gmail.com>
2023-03-19 19:48:18 +0700

    Fetch menu

--- frontend/src/resources/routes/Client.js
+++ frontend/src/resources/routes/Client.js
@@ -7,9 +7,43 @@ const API_URL = process.env.API_URL;
 const { upload } = require('../middlewares/multer');
 
 router.get('/menu', async (req, res, next) => {
+    await fetch(API_URL + "/products/get-menu", {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json'}
+    })
+    .then(async result => {
+        result = await result.json();
+
+        if(result.success) {
+            let data = result.data;
+            let orders = [];
+            if(data.length != 0) {
+                orders = data.map(d => {
+                    return {
+                        pid: d.product_ID,
+                        pname: d.product_name,
+                        pimg: d.image_link,
+                        price: d.product_price.toString().toLocaleString('vi-vn'),
+                    }
+                })
+            }
+
+            return res.render('pages/clients/menu', {
+                layout: 'main',
+                success: req.flash('success') || '',
+                error: req.flash('error') || '',
+                orders: orders
+            })
+        }
+    })
+
     return res.render('pages/clients/menu', {
         layout: 'main'
     })
+})
+
+router.get('/place-order', async (req, res, next) => {
+    
 })
 
 module.exports = router;
