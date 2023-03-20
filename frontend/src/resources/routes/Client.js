48fa19316ea4f2681d4d4fdd5c2392819dfd8e3e
Lê Gia Phú <520H0401@student.tdtu.edu.vn>
2023-03-20 16:59:18 +0700

    Revert "diagram"
    This reverts commit ce18cb0dad7fe242cc69a151adf62484a0ae7455.

--- frontend/src/resources/routes/Client.js
+++ frontend/src/resources/routes/Client.js
@@ -6,24 +6,24 @@ const fileapis = require('../middlewares/fileapis');
 const API_URL = process.env.API_URL;
 const { upload } = require('../middlewares/multer');
 
-
 router.get('/menu', async (req, res, next) => {
     await fetch(API_URL + "/products/get-menu", {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' }
     })
-        .then(async (result) => {
+        .then(async result => {
             result = await result.json();
-            if (result.success == true) {
-                let products = result.data;
+
+            if (result.success) {
+                let data = result.data;
                 let orders = [];
-                if (products.length != 0) {
-                    orders = products.map(p => {
+                if (data.length != 0) {
+                    orders = data.map(d => {
                         return {
-                            pid: p.product_ID,
-                            pname: p.product_name,
-                            pimg: p.image_link,
-                            price: p.product_price.toString().toLocaleString('vi-vn'),
+                            pid: d.product_ID,
+                            pname: d.product_name,
+                            pimg: d.image_link,
+                            price: d.product_price.toString().toLocaleString('vi-vn'),
                         }
                     })
                 }
@@ -36,20 +36,14 @@ router.get('/menu', async (req, res, next) => {
                 })
             }
         })
-        .catch((err) => {
-            return res.status(500).json({err})
-        })
 
-    // return res.render('pages/clients/menu', {
-    //     layout: 'main'
-    // })
+    return res.render('pages/clients/menu', {
+        layout: 'main'
+    })
 })
 
-
 router.get('/place-order', async (req, res, next) => {
-    // console.log(req)
-    console.log(req.data)
-    await fetch(API_URL + 'product')
+
 })
 
 module.exports = router;
